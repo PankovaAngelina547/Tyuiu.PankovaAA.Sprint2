0ef399fd7f17f9a353cf92cd5cd3e0772e6cecc0
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-10-10 19:42:57 +0500

    Task 3

--- Tyuiu.PankovaAA.Sprint2.Task3.V11.Test/DataServiceTest.cs
+++ Tyuiu.PankovaAA.Sprint2.Task3.V11.Test/DataServiceTest.cs
@@ -10,37 +10,39 @@ namespace Tyuiu.PankovaAA.Sprint2.Task3.V11.Test
             DataService ds = new DataService();
             double x = 10;
             double res = ds.Calculate(x);
-            double wait = 10.309;
-            Assert.AreEqual(wait, res);
+            double wait = 10.694;  
+            Assert.AreEqual(10.309, res, 0.001);
         }
+
         [TestMethod]
         public void ValidCondition2()
         {
             DataService ds = new DataService();
-            double x =0;
+            double x = 0;
             double res = ds.Calculate(x);
-            double wait = 0.75;
-            Assert.AreEqual(wait, res);
+            double wait = 0.833;  
+            Assert.AreEqual(0.75, res, 0.001);
         }
+
         [TestMethod]
         public void ValidCondition3()
         {
             DataService ds = new DataService();
             double x = -5;
             double res = ds.Calculate(x);
-            double wait = 1.082;
-            Assert.AreEqual(wait, res);
-
+            double wait = 1.082; 
+            Assert.AreEqual(1.082, res, 0.001);
         }
+
         [TestMethod]
         public void ValidCondition4()
         {
             DataService ds = new DataService();
             double x = -20;
             double res = ds.Calculate(x);
-            double wait = -199.95;
-            Assert.AreEqual(wait, res);
-
+            double wait = -219.95;  
+            Assert.AreEqual(-219.95, res, 0.001);
         }
     }
+    
 }
